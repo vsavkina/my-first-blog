ef84b8caad1025e02c8da41797a782596edb319c
vsavkina <savkinava@yandex.ru>
2018-11-19 23:42:51 +0300

    2nd commit

--- msite/urls.py
+++ msite/urls.py
@@ -1,4 +1,6 @@
-"""msite URL Configuration
+"""
+***I CHANGED THIS***
+msite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
